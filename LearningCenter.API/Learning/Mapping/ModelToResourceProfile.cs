2260447997505f28fb374c9a7aaf5954f9549ebf
2CTO <u201521691@upc.edu.pe>
2022-06-07 16:59:35 -0500

    feat: category updating

---
+++ LearningCenter.API/Learning/Mapping/ModelToResourceProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using LearningCenter.API.Learning.Domain.Models;
+using LearningCenter.API.Learning.Domain.Services.Communication;
+using LearningCenter.API.Learning.Resources;
+
+namespace LearningCenter.API.Learning.Mapping;
+
+public class ModelToResourceProfile : Profile
+{
+    public ModelToResourceProfile()
+    {
+        CreateMap<Category, CategoryResource>();
+    }
+}
