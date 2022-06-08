2daa85a598488a802941176288fadcbe0e4d2004
2CTO <u201521691@upc.edu.pe>
2022-06-08 06:17:54 -0500

    feat: finishing categories

--- LearningCenter.API/Learning/Resources/SaveCategoryResource.cs
+++ LearningCenter.API/Learning/Resources/SaveCategoryResource.cs
@@ -1,6 +1,10 @@
-﻿namespace LearningCenter.API.Learning.Resources;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LearningCenter.API.Learning.Resources;
 
 public class SaveCategoryResource
 {
+    [Required]
+    [MaxLength(30)]
     public string Name { get; set; }
 }
